e70419c336baf7006b1987e0bc2b503279cbe83c
Joao Victor Sena <jvsena16@gmail.com>
2025-04-13 18:30:35 -0300

    feat: enable edge to edge

--- app/src/main/java/com/github/luckyhash/MainActivity.kt
+++ app/src/main/java/com/github/luckyhash/MainActivity.kt
@@ -1,26 +1,21 @@
 package com.github.luckyhash
 
+import android.Manifest
+import android.content.Intent
 import android.os.Build
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
-import com.github.luckyhash.ui.theme.LuckyHashTheme
 import androidx.activity.result.contract.ActivityResultContracts
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
+import androidx.compose.ui.Modifier
 import androidx.navigation.compose.rememberNavController
-import com.github.luckyhash.ui.navigation.AppNavHost
-import android.Manifest
-import android.content.Intent
 import com.github.luckyhash.domain.MiningService
+import com.github.luckyhash.ui.navigation.AppNavHost
+import com.github.luckyhash.ui.theme.LuckyHashTheme
 
 class MainActivity : ComponentActivity() {
 
@@ -36,7 +31,7 @@ class MainActivity : ComponentActivity() {
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU) {
             requestPermissionLauncher.launch(Manifest.permission.POST_NOTIFICATIONS)
         }
-
+        enableEdgeToEdge()
         setContent {
             LuckyHashTheme {
                 Surface(
